ed5733d6d8578bef8a1244e2c7f9b3f49d8489df
nohan24 <nohanrazafinjato@gmail.com>
2022-12-21 18:00:32 +0300

    base

--- base.sql
+++ base.sql
@@ -1,1 +1,13 @@
+
 CREATE DATABASE immo;
+
+CREATE TABLE users (idUser SERIAL PRIMARY KEY, pseudo VARCHAR(100), email VARCHAR(100), mdp VARCHAR(50), user_type SMALLINT);
+
+CREATE SEQUENCE userseq;
+
+SELECT SETVAL('userseq', 1);
+
+SELECT NEXTVAL('userseq');
+
+INSERT INTO USER VALUES (1, 'Admin', 'admin@gmail.com', 'root', 1);
+
